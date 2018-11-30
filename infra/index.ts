f29fe33e9d77cf94f06f9093913fe7b21539d5e8
Benjamin Villanueva <benjysandra@hotmail.com>
2018-11-30 15:50:14 -0600

    testing vnet

--- infra/index.ts
+++ infra/index.ts
@@ -2,11 +2,14 @@ import * as k8s from "@pulumi/kubernetes";
 import * as azure from "@pulumi/azure";
 import * as config from "./config";
 
+// Create Virtual Network
+import {vnetID} from "./vnet";
+
 // Create an AKS cluster.
-import { k8sCluster, k8sProvider } from "./cluster";
+//import { k8sCluster, k8sProvider } from "./cluster";
 
 // Export kubeconfig file, cluster name, and public IP address for Kubernetes application. These can
 // be accessed from the CLI, like: `pulumi stack output kubeconfig > kubeconfig.yaml`.
-export const kubeconfig = k8sCluster.kubeConfig;
-export const cluster = k8sCluster.name;
+//export const kubeconfig = k8sCluster.kubeConfig;
+//export const cluster = k8sCluster.name;
 
